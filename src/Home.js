d20940279f446c12a1fef81b7005300afa57a513
Siful Islam Marzan <142264095+abyanmarzan@users.noreply.github.com>
2024-12-08 19:35:37 +0600

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -127,7 +127,7 @@ const Home = () => {
                 </div>
                 <Slider {...settings}>
                     {movies.map((movie) => (
-                        <div key={movie.id} className="px-1">
+                        <div key={movie.id} className="px-2">
                             <Card
                                 id={movie.id}
                                 name={movie.title}
